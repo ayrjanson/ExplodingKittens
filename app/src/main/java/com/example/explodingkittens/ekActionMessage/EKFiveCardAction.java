7066895234f56ba3d0b77e593006c28e90a13121
ayrjanson <yrjanson24@up.edu>
2021-11-04 20:58:41 -0700

    EKActionClasses

---
+++ app/src/main/java/com/example/explodingkittens/ekActionMessage/EKFiveCardAction.java
@@ -0,0 +1,8 @@
+package com.example.explodingkittens.ekActionMessage;
+
+import com.example.gameframework.actionMessage.GameAction;
+import com.example.gameframework.players.GamePlayer;
+
+public class EKFiveCardAction extends GameAction {
+    public EKFiveCardAction(GamePlayer player) { super(player); }
+}
