8720f8208f008d8dc8b35e0b65302a94d0969320
nastase24 <89493347+nastase24@users.noreply.github.com>
2021-11-10 01:19:43 -0800

    EKHUMANPLAYER: onClick - implemented the playerCard functions, and the handLeft and handRight function.  recieveInfo - modified the last else branch to use CurrIdx to set the correct image if one cycles left or right. Implemented hashMap called buttonCardMap<Integer,CARDTYPE> to link the playerCard ID's to the CARDTYPE of the card it represents. EKComputerPlayer1.java: recieveInfo - modified the method in which actions are sent around - still dubious about if this is going to work properly. EKLocalGame.java: modified the makeMove method to use the cardtype of the EKPlayCardAction to execute actions in the switch statement NOTE: endturn and draw are now members of the cardtype enum. EKPlayCardAction: new class that deals with all of the actions to be done, has a CARDTYPE cariable that needs to be in the constructor BTW -deleted the obsolete action Classes TODO: clicking the card removes it from the hand and sets the discard pile to that image - unsure if it sets the right image everytime, test that out -check if draw card functionality will add the card, and if it already does, display it in the hand

--- app/src/main/java/com/example/explodingkittens/ekActionMessage/EKAttackAction.java
+++
@@ -1,9 +0,0 @@
-package com.example.explodingkittens.ekActionMessage;
-
-import com.example.gameframework.actionMessage.GameAction;
-import com.example.gameframework.players.GamePlayer;
-
-public class EKAttackAction extends GameAction {
-
-    public EKAttackAction(GamePlayer player) { super(player); }
-}
